356c44adfbc77a88e480bd4de50ee9c844ff3a5f
am-cid <mcidantonio@gmail.com>
2023-10-21 23:05:42 +0800

    finished place_queen + removed redundant add_queen

--- algorithm/eight_queens.py
+++ algorithm/eight_queens.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 
 class EightQueens():
-    """
-    !REMOVE THIS!
-    structure:
-        1. properties first
-        2. public methods next
-        3. private methods last
-    """
+    'description'
 
     def __init__(self, size: int = 8):
         self._size = size
         self._queen_positions = []
         self._board = [[{}]]
+        """ temporary _generate_board for testing
+        self._board = [[{
+            "id": None,
+            "value": 0,
+            "asset": None
+        } for _ in range(8)] for _ in range(8)]
+        """
+        self._valid_queen_asset = Path("path/to/valid/queen/asset")
+        self._invalid_queen_asset = Path("path/to/invalid/queen/asset")
+        self._killzone_asset = Path("path/to/killzone/asset")
+
         self._generate_board()
     
     @property
@@ -29,12 +34,62 @@ class EightQueens():
     def queens(self) -> list[tuple[int,int]]:
         return self._queen_positions
     
-    def add_queen(self, position: tuple[int,int]):
-        self._validate_position(position)
-        self._queen_positions.append(position)
-    
     def place_queen(self, position: tuple):
         self._validate_position(position)
+        self._queen_positions.append(position)
+
+        row, column = position
+        self._board[row][column]["id"] = "q"
+
+        # row and column killzone
+        for i in range(self.size):
+            # rows
+            placed_queen = column
+            if i != placed_queen:
+                self._board[row][i]["id"] = "k"
+                self._board[row][i]["value"] += 1
+                self._board[row][i]["asset"] = self._killzone_asset
+
+            # columns
+            placed_queen = row
+            if i != placed_queen:
+                self._board[i][column]["id"] = "k"
+                self._board[i][column]["value"] += 1
+                self._board[i][column]["asset"] = self._killzone_asset
+        
+        # quadrant 1 (upper right)
+        i=1
+        while row-i>=0 and column+i<self.size:
+            self._board[row-i][column+i]["id"] = "k"
+            self._board[row-i][column+i]["value"] += 1
+            self._board[row-i][column+i]["asset"] = self._killzone_asset
+            i += 1
+
+        # quadrant 2 (upper left)
+        i=1
+        while row-i>=0 and column-i>=0:
+            self._board[row-i][column-i]["id"] = "k"
+            self._board[row-i][column-i]["value"] += 1
+            self._board[row-i][column-i]["asset"] = self._killzone_asset
+            i += 1
+
+        # quadrant 3 (bottom right)
+        i=1
+        while row+i<self.size and column-i>=0:
+            self._board[row+i][column-i]["id"] = "k"
+            self._board[row+i][column-i]["value"] += 1
+            self._board[row+i][column-i]["asset"] = self._killzone_asset
+            i += 1
+
+        # quadrant 4 (bottom left)
+        i=1
+        while row+i<self.size and column+i<self.size:
+            self._board[row+i][column+i]["id"] = "k"
+            self._board[row+i][column+i]["value"] += 1
+            self._board[row+i][column+i]["asset"] = self._killzone_asset
+            i += 1
+
+        self._validate_queens()
 
     def remove_queen(self, position: tuple):
         self._validate_position(position)
