b2965984b918f7c785e4ad2d053439ed98b5d2b8
am-cid <mcidantonio@gmail.com>
2023-10-22 10:22:14 +0800

    adding and removing killzones are now subfunctions

--- algorithm/eight_queens.py
+++ algorithm/eight_queens.py
@@ -45,79 +45,67 @@ class EightQueens():
             ValueError: If the specified position is already occupied by another queen.
         """
         self._validate_position(position)
-        
+        row, column = position
+
         occupied = self._board[row][column]["id"] == "queen"
         if occupied:
             ALREADY_OCCUPIED = f"This tile ({row},{column}) already has a queen. It has {self._board[row][column]['id']} as the id\nDid you mean to use 'remove_queen()' instead?"
             raise ValueError(ALREADY_OCCUPIED)
         
         self._queen_positions.append(position)
-
-        row, column = position
         self._board[row][column]["id"] = "queen"
 
-        def tile_is_empty(row: int, column: int) -> bool:
-            "helper subfunction to avoid replacing a queen's id with a killzone from other queens"
-            return self._board[row][column]["id"] != "queen"
+        def add_killzone(row: int, column: int):
+            '''
+            helper subfunction to add a killzone based on position given
+            also checks if tile is empty beforehand to avoid replacing a queen's id with a killzone from other queens
+            '''
+            is_tile_empty = self._board[row][column]["id"] != "queen"
+            if is_tile_empty:
+                self._board[row][column]["id"] = "killzone"
+                self._board[row][column]["asset"] = self._killzone_asset
+
 
         # row and column killzone
         for i in range(self.size):
+            self._board[row][i]["value"] += 1
+
             # rows
             placed_queen = column
             if i != placed_queen:
-                self._board[row][i]["value"] += 1
-
-                if tile_is_empty(row, i):
-                    self._board[row][i]["id"] = "killzone"
-                    self._board[row][i]["asset"] = self._killzone_asset
+                add_killzone(row, i)
 
             # columns
             placed_queen = row
             if i != placed_queen:
-                self._board[i][column]["value"] += 1
-
-                if tile_is_empty(i, column):
-                    self._board[i][column]["id"] = "killzone"
-                    self._board[i][column]["asset"] = self._killzone_asset
+                add_killzone(i, column)
         
         # quadrant 1 (upper right)
         i=1
         while row-i>=0 and column+i<self.size:
             self._board[row-i][column+i]["value"] += 1
-
-            if tile_is_empty(row-i, column+i):
-                self._board[row-i][column+i]["id"] = "killzone"
-                self._board[row-i][column+i]["asset"] = self._killzone_asset
+            add_killzone(row-i, column+i)
             i += 1
 
         # quadrant 2 (upper left)
         i=1
         while row-i>=0 and column-i>=0:
             self._board[row-i][column-i]["value"] += 1
-
-            if tile_is_empty(row-i, column-i):
-                self._board[row-i][column-i]["id"] = "killzone"    
-                self._board[row-i][column-i]["asset"] = self._killzone_asset
+            add_killzone(row-i, column-i)
             i += 1
 
         # quadrant 3 (bottom right)
         i=1
         while row+i<self.size and column-i>=0:
             self._board[row+i][column-i]["value"] += 1
-
-            if tile_is_empty(row+i, column-i):
-                self._board[row+i][column-i]["id"] = "killzone"
-                self._board[row+i][column-i]["asset"] = self._killzone_asset
+            add_killzone(row+i, column-i)
             i += 1
 
         # quadrant 4 (bottom left)
         i=1
         while row+i<self.size and column+i<self.size:
             self._board[row+i][column+i]["value"] += 1
-
-            if tile_is_empty(row+i, column+i):
-                self._board[row+i][column+i]["id"] = "killzone"
-                self._board[row+i][column+i]["asset"] = self._killzone_asset
+            add_killzone(row+i, column+i)
             i += 1
 
         self._validate_queens()
@@ -133,6 +121,7 @@ class EightQueens():
             ValueError: If the specified position is not occupied by a queen.
         """
         self._validate_position(position)
+        row, column = position
         
         occupied = self._board[row][column]["id"] == "queen"
         if not occupied:
@@ -141,75 +130,62 @@ class EightQueens():
         
         self._queen_positions.pop(self._queen_positions.index(position))
 
-        row, column = position
         self._board[row][column]["id"] = None
 
         removed_queen_was_killzone = self._board[row][column]["value"] > 0
         if removed_queen_was_killzone:
             self._board[row][column]["id"] = "killzone"
 
-        def removed_killzone_is_empty(row: int, column: int) -> bool:
-            'helper subfunction to avoid replacing asset of an invalid queen when removing a killzone'
-            return self._board[row][column]["value"] == 0 and self._board[row][column]["asset"] == self._killzone_asset
+        def remove_killzone(row: int, column: int):
+            '''
+            helper subfunction to remove a killzone based on position given
+            also to avoid replacing asset of an invalid queen when removing a killzone
+            '''
+            is_removed_killzone_empty = self._board[row][column]["value"] == 0 and self._board[row][column]["asset"] == self._killzone_asset
+            if is_removed_killzone_empty:
+                self._board[row][column]["id"] = None
+                self._board[row][column]["asset"] = None
 
         # row and column killzone
         for i in range(self.size):
+            self._board[row][i]["value"] -= 1
+
             # rows
             removed_queen = column
             if i != removed_queen:
-                self._board[row][i]["value"] -= 1
-
-                if removed_killzone_is_empty(row, i):
-                    self._board[row][i]["id"] = None
-                    self._board[row][i]["asset"] = None
+                remove_killzone(row, i)
 
             # columns
             removed_queen = row
             if i != removed_queen:
-                self._board[i][column]["value"] -= 1
-
-                if removed_killzone_is_empty(i,column):
-                    self._board[i][column]["id"] = None
-                    self._board[i][column]["asset"] = None
+                remove_killzone(i, column)
         
         # quadrant 1 (upper right)
         i=1
         while row-i>=0 and column+i<self.size:
             self._board[row-i][column+i]["value"] -= 1
-
-            if removed_killzone_is_empty(row-i, column+i):
-                self._board[row-i][column+i]["id"] = None
-                self._board[row-i][column+i]["asset"] = None
+            remove_killzone(row-i, column+i)
             i += 1
 
         # quadrant 2 (upper left)
         i=1
         while row-i>=0 and column-i>=0:
             self._board[row-i][column-i]["value"] -= 1
-            
-            if removed_killzone_is_empty(row-i, column-i):
-                self._board[row-i][column-i]["id"] = None
-                self._board[row-i][column-i]["asset"] = None
+            remove_killzone(row-i, column-i)
             i += 1
 
         # quadrant 3 (bottom right)
         i=1
         while row+i<self.size and column-i>=0:
             self._board[row+i][column-i]["value"] -= 1
-            
-            if removed_killzone_is_empty(row+i, column-i):
-                self._board[row+i][column-i]["id"] = None
-                self._board[row+i][column-i]["asset"] = None
+            remove_killzone(row+i, column-i)
             i += 1
 
         # quadrant 4 (bottom left)
         i=1
         while row+i<self.size and column+i<self.size:
             self._board[row+i][column+i]["value"] -= 1
-            
-            if removed_killzone_is_empty(row+i, column+i):
-                self._board[row+i][column+i]["id"] = None
-                self._board[row+i][column+i]["asset"] = None
+            remove_killzone(row+i, column+i)
             i += 1
 
         self._validate_queens()
